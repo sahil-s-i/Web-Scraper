6fd86042718188cf1873837007aef6f648f540ff
Sahil <sahilismailnavar@gmail.com>
2023-10-22 19:18:22 +0530

    Added comments to the code

--- main.py
+++ main.py
@@ -3,14 +3,17 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 proxies = {
+    #Add a Rotating proxies To avoid the risk of getting blocked.
     "http": "http://10.10.1.10:3128",
     "https": "https://10.10.1.10:1080"
 }
 
+# Data to be stored in the file
 data = {'Title': [],
         'Price': [],
         'Image': []}
 
+# Change This url According to the needs
 url = "https://www.amazon.in/s?k=iphones&crid=2V86TE8OVY1XC&sprefix=iphone%2Caps%2C495&ref=nb_sb_noss_2"
 headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
@@ -42,7 +45,9 @@ print(f'url={len(images)}\ntitles={len(spans)}\nprices={len(prices)}')
 
 # Check if all lists have the same length before creating the DataFrame
 if len(data["Title"]) == len(data["Price"]) == len(data["Image"]):
+    # Create a data frame
     df = pd.DataFrame(data)
-    df.to_csv("data.csv", index=False)
+    # Save a data to the data.excel file
+    df.to_excel("data.xlsx", index=False)
 else:
     print("The lists have different lengths, cannot create DataFrame.")
